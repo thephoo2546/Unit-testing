58d10e56e24ed0c8155cef8e2cb4d1705c9f3929
thephoo2546 <thephoo7@gmail.com>
2023-10-11 00:14:53 +0700

    Unit-testing

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage></InputPage>
+  <FunctionPage></FunctionPage>
+</template>
+
+
+<script>
+import InputPage from "src/pages/InputPage.vue"
+import FunctionPage from "src/pages/FunctionPage.vue"
+export default {
+  name: 'HtmlComponent',
+    components:{
+      InputPage,
+      FunctionPage
+    },
+  data() {
+    return {
+      title: 'พิริยะ อินขาน',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
